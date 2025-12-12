9abb7eddf0803c8d0cec3c9fede7bdc526eec36c
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-12-13 03:10:46 +0530

    setu[ e2b sandboxes and runn next js apps

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,18 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "e2b";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "agent/hello" },
-  async () => {
+  async ({event,step}) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async (): Promise<string> => {
+      const sandbox = await Sandbox.create("bolt")
+      return sandbox.sandboxId
+    })
+
+
     const helloAgent = createAgent({
       name: "hello-agent",
       description: "A simple agent that greets the user.",
@@ -17,8 +25,14 @@ export const helloWorld = inngest.createFunction(
     const firstMessage = result.output[0];
 console.log("firstMessage",firstMessage)
     const text =
-      firstMessage?.content
+      firstMessage?.content 
       // console.log(text)
+
+      const sandboxUrl = await step.run("get-sandbox-url",async()=>{
+        const sandbox = await Sandbox.connect(sandboxId)
+        const host = sandbox.getHost(3000)
+        return `http://${host}`
+      }) 
     return {
       text,
     };
